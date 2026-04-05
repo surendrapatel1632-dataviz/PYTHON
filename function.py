cae15e0734dbc46dcf36cd67f16a62699ba8852b
Surendra1632 <surendra.lateral2k22@mpgi.edu.in>
2026-04-05 18:49:03 +0530

    find the even or odd

---
+++ function.py
@@ -0,0 +1,8 @@
+# find the even and odd number
+def even_or_odd(num):
+    """find the number even or odd"""
+    if num%2==0:
+        print("the number is even")
+    else:
+        print("the number is odd") 
+even_or_odd(24)       
