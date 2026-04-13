3462621ab53c8c106a44db3184de8138a560df1e
Surendra1632 <surendra.lateral2k22@mpgi.edu.in>
2026-04-13 22:34:58 +0530

    check the daimention array

---
+++ numpy1.py
@@ -0,0 +1,5 @@
+#creating the array passing list
+import numpy as np
+n=np.array([1,2,3,4,5])
+print(n)
+print(type(n))
---
+++ numpy2.py
@@ -0,0 +1,8 @@
+#create the array check the daimention array
+import numpy as np
+n=np.array([10,29,30])
+s=np.array(([1,2,3],[3,4,5]))
+print(n)
+print("daimenstion",n.ndim)
+print(s)
+print("daimenstion",s.ndim)
