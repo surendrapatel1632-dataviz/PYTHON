4e33924fa5b51738dd9b6534ae9fc5d0510c4b49
Surendra1632 <surendra.lateral2k22@mpgi.edu.in>
2026-04-16 07:12:45 +0530

    3D array

---
+++ numpy10.py
@@ -0,0 +1,11 @@
+# access element from 3d array
+import numpy as np
+n=np.array([[[5,10,15],[20,25,35]],[[35,40,45],[50,66,60]]])
+print(n)
+print(n[0,0,0])
+print(n[0,0,1])
+print(n[0,0,2])
+print(n[1,1,1])
+print(n[1,1,2])
+print(n[0,1,2])
+print(n[0,1,0])
