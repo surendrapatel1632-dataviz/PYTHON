8f5360a6174566269c41f857523e162663686c40
Surendra1632 <surendra.lateral2k22@mpgi.edu.in>
2026-04-17 07:21:42 +0530

    sliceing the index

---
+++ numpy14.py
@@ -0,0 +1,5 @@
+#slicing the array 1 to 3
+import numpy as np
+n=np.array([20,25,30,35,40,45,50,55,60,65])
+print(n)
+print(n[1:3])
---
+++ numpy15.py
@@ -0,0 +1,5 @@
+#slicing from index 2 to 5 
+import numpy as np
+n=np.array([20,25,30,35,40,45,50])
+print(n)
+print(n[2:5])
