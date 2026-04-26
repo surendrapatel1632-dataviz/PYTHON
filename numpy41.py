557dc7cf7fffddb6c527139428f76bf0d92762cd
Surendra1632 <surendra.lateral2k22@mpgi.edu.in>
2026-04-26 18:21:15 +0530

    addition

---
+++ numpy41.py
@@ -0,0 +1,10 @@
+# addition opration for numpy array
+import numpy as np
+n1=np.array([15,20,25,30])
+n2=np.array([65,75,85,95])
+print("array1=",n1)
+print("array2=",n2)
+n1=n1+10;
+n2=n2+10;
+print("updated array1=",n1)
+print("updated array2=",n2)
